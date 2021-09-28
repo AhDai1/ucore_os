304db829373c1cbc10cf312b4ca298700bf2b8a9
AhDai1 <1305377980@qq.com>
2021-09-28 14:21:27 +0800

    add lab5 codes

--- labcodes/lab5/kern/trap/trap.c
+++ labcodes/lab5/kern/trap/trap.c
@@ -15,6 +15,8 @@
 #include <error.h>
 #include <sched.h>
 #include <sync.h>
+//////#include<../libs/unistd.h>
+//#include<../process/proc.h>
 
 #define TICK_NUM 100
 
@@ -53,9 +55,14 @@ idt_init(void) {
       *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
       *     Notice: the argument of lidt is idt_pd. try to find it!
       */
-     /* LAB5 YOUR CODE */ 
-     //you should update your lab1 code (just add ONE or TWO lines of code), let user app to use syscall to get the service of ucore
-     //so you should setup the syscall interrupt gate in here
+    extern uintptr_t __vectors[];
+    int i;
+    for (i = 0; i < sizeof(idt) / sizeof(struct gatedesc); i ++) {
+        SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
+    }
+    SETGATE(idt[T_SWITCH_TOK], 0, GD_KTEXT, __vectors[T_SWITCH_TOK], DPL_USER);
+    SETGATE(idt[T_SYSCALL], 1, GD_KTEXT,  __vectors[T_SYSCALL], DPL_USER);
+    lidt(&idt_pd);
 }
 
 static const char *
@@ -224,6 +231,13 @@ trap_dispatch(struct trapframe *tf) {
          *    Every TICK_NUM cycle, you should set current process's current->need_resched = 1
          */
   
+	
+	    ticks ++;
+        if (ticks % TICK_NUM == 0) {
+           // print_ticks();
+           assert(current != NULL);
+           current->need_resched = 1;
+        }
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
